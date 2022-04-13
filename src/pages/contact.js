6f3493ea44162dab6146ccb574eff8203cd1f3ad
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2022-04-13 16:07:28 -0500

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -22,22 +22,18 @@ const ContactPage = ({
             contact@doctorlawyerengineer.com</a></b>!</p>
         </div>
         <div>
-          <form className="form-container" name="contact" data-netlify="true">
+          <form className="form-container" name="contact" method="POST" data-netlify="true">
             <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName" />
+              <label>Name <input type="text" name="name" /></label>
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender" />
+              <label>Email <input type="email" name="email" /></label>
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject" />
+              <label>Subject <input type="text" name="subject"/></label>
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage" />
+              <label>Message <textarea name="message"></textarea></label>
             </div>
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
               <input type="submit" className="button -primary" style={{ marginRight: 0 }} />
