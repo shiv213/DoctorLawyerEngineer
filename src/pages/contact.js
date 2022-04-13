d615715db332bef07786ee1e8b58471b3ab0124a
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2022-04-13 13:04:50 -0500

    form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,29 +18,25 @@ const ContactPage = ({
         <div className="post-thumbnail"
              style={{ backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0 }}>
           <h1 className="post-title">Get in Touch</h1>
-          <p>Reach out using the form or email us at <b>contact@doctorlawyerengineer.com</b>! &rarr;</p>
+          <p>Reach out using the form or email us at <b><a
+            href="mailto:contact@doctorlawyerengineer.com?subject=DLE Contact Form&body=Message">
+            contact@doctorlawyerengineer.com</a></b>!</p>
         </div>
         <div>
-          <form className="form-container" name="contact" netlify>
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName" />
-            </div>
-            <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender" />
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject" />
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage" />
-            </div>
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-              <input type="submit" className="button -primary" style={{ marginRight: 0 }} />
-            </div>
+          <form className="contact-form" method="POST" name="contact"><input type="hidden" name="form-name"
+                                                                             value="contact"><label
+            htmlFor="name">Name</label> <input type="text" name="name" id="name" autoComplete="name"
+                                               placeholder="Your name" title="Please enter your name" required="">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" autoComplete="email"
+                   placeholder="Your email address"
+                   title="The domain portion of the email address is invalid (the portion after the @)."
+                   pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$"
+                   required="">
+              <label htmlFor="message">Message</label>
+              <textarea
+                name="message" id="message" placeholder="Write your message here" rows="7" required="" />
+              <button className="contact-form__submit btn" type="submit" name="submit">Send Message</button>
           </form>
         </div>
       </div>
@@ -49,7 +45,7 @@ const ContactPage = ({
 };
 export default ContactPage;
 export const pageQuery = graphql`
-  query ContactPageQuery{
+query ContactPageQuery{
     site {
       siteMetadata {
         title
